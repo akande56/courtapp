bd382138839276b497608dd43c495e6e1c10d8b0
akande56 <abdulsalamabubakar52@gmail.com>
2023-07-14 10:19:17 +0100

    removed add user social account

--- courtapp/users/adapters.py
+++ courtapp/users/adapters.py
@@ -21,17 +21,17 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
     def is_open_for_signup(self, request: HttpRequest, sociallogin: SocialLogin) -> bool:
         return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
 
-    def populate_user(self, request: HttpRequest, sociallogin: SocialLogin, data: dict[str, typing.Any]) -> User:
-        """
-        Populates user information from social provider info.
+    # def populate_user(self, request: HttpRequest, sociallogin: SocialLogin, data: dict[str, typing.Any]) -> User:
+    #     """
+    #     Populates user information from social provider info.
 
-        See: https://django-allauth.readthedocs.io/en/latest/advanced.html?#creating-and-populating-user-instances
-        """
-        user = sociallogin.user
-        if (name := data.get("name")):
-            user.name = name
-        elif first_name := data.get("first_name"):
-            user.name = first_name
-            if last_name := data.get("last_name"):
-                user.name += f" {last_name}"
-        return super().populate_user(request, sociallogin, data)
+    #     See: https://django-allauth.readthedocs.io/en/latest/advanced.html?#creating-and-populating-user-instances
+    #     """
+    #     user = sociallogin.user
+        # if name := data.get("name"):
+        #     user.name = name
+        # elif first_name := data.get("first_name"):
+        #     user.name = first_name
+        #     if last_name := data.get("last_name"):
+        #         user.name += f" {last_name}"
+        # return super().populate_user(request, sociallogin, data)
